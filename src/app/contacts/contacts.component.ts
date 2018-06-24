6dbcf9b0b0ac888250a12a353fd29c88b994739b
PragnaMunagala <pmunagal@asu.edu>
2018-06-24 12:08:51 -0700

    Source code changes for contacts widget

---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css', '../app.component.css']
+})
+export class ContactsComponent implements OnInit {
+  contacts: any = [];
+  optionType = ['Email address', 'Phone number'];
+  type = 'Email address';
+  displayDetails = [];
+  clicked = false;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('assets/data.json').subscribe((data) =>  {
+      this.contacts = data;
+      for (let i = 0; i < this.contacts.length; i++) {
+        this.displayDetails[i] = false;
+      }
+    });
+  }
+
+  dataTypeChange(option) {
+    this.type = option;
+    console.log(option);
+  }
+
+  detailsData(index) {
+    this.displayDetails[index] = !this.displayDetails[index];
+    this.displayDetails.map((data, ind) => {
+      if (ind !== index) {
+        this.displayDetails[ind] = false;
+      }
+    });
+
+    if (this.displayDetails.includes(true)) {
+      this.clicked = true;
+    } else {
+      this.clicked = false;
+    }
+  }
+
+  outOfFocus() {
+    return this.displayDetails.map((data, ind) => {
+      data = false;
+      return data;
+    });
+  }
+
+}
